8037026b98fdd06c69042b4a980fde90f2e5c34d
Daniel Silva <danielsilvatsi@outlook.com>
2024-06-20 18:59:47 -0300

    feat: remaining controller implementations

--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -1,4 +1,15 @@
-﻿namespace Catalog.API
+﻿using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Data.Repositories;
+using HealthChecks.UI.Client;
+using MediatR;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Catalog.API
 {
     public class Startup
     {
@@ -11,6 +22,20 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
+           
+            services.AddApiVersioning();
+            services.AddHealthChecks()
+           .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "Catalog  Mongo Db Health Check",
+               HealthStatus.Degraded);
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
+
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBrandRepository, ProductRepository>();
+            services.AddScoped<ITypesRepository, ProductRepository>();
+
             services.AddControllers();
         }
 
@@ -19,14 +44,22 @@
             if (env.IsDevelopment())
             { 
               app.UseDeveloperExceptionPage();
+              app.UseSwagger();
+              app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
             }
 
             app.UseRouting();
+            app.UseAuthentication();
             app.UseStaticFiles();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
