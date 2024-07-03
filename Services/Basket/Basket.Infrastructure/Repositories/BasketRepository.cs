5a4024ddf8057df8c32fb8da747cab61aa2ccf83
Daniel Silva <danielsilvatsi@outlook.com>
2024-07-03 19:38:59 -0300

    feat: basket repository implementation

---
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCahe;
+
+        public BasketRepository(IDistributedCache redisCahe)
+        {
+            _redisCahe = redisCahe;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCahe.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }              
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _redisCahe.SetStringAsync(shoppingCart.UserName, JsonConvert.SerializeObject(shoppingCart));
+            return await GetBasket(shoppingCart.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await  _redisCahe.RemoveAsync(userName);
+        }
+    }
+}
