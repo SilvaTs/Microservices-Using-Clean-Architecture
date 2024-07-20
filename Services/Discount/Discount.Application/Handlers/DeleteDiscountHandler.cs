125ae91b4354b633419ec7f8f320aacef1937d61
Daniel Silva <danielsilvatsi@outlook.com>
2024-07-19 22:58:53 -0300

    feat: delete discount command and handler

---
+++ Services/Discount/Discount.Application/Handlers/DeleteDiscountHandler.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Handlers;
+
+public class DeleteDiscountHandlerRG : IRequestHandler<DeleteDiscountCommand, bool>
+{
+    private readonly IDiscountRepository _discountRepository;
+
+    public DeleteDiscountHandlerRG(IDiscountRepository discountRepository)
+    {
+        _discountRepository = discountRepository;
+    }
+
+    public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        var deleted = await _discountRepository.DeleteDiscount(request.ProductName);
+        return deleted;
+    }
+}
